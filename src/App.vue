61086e8b617efc27293f9671a03b61cd5f694c68
jjzjx118 <jjzjx118@qq.com>
2025-02-28 00:41:07 +0800

    烦死了~！！！烦死了~！！！烦死了~！！！烦死了~！！！

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,85 @@
 <template>
-    <el-container>
-      <el-header class="header" style="height: 500px"><div class="header">五维农业</div> </el-header>
-      <el-main class="main">
-      
-      </el-main>
-      <el-footer class="footer">Footer</el-footer>
-    </el-container>
+  <el-container>
+    <el-header class="header" >
+      <div class="header">
+        <!-- <div class="demo-image__lazy"> -->
+          <!-- 使用变量绑定图片路径 -->
+           <div class="navigator">
+
+           </div>
+       
+          <img class="img" src="@/assets/image/bgd_2.png" alt="本地图片" />
+        <!-- </div> -->
+      </div>
+    </el-header>
+    <el-main class="main"> </el-main>
+    <el-footer class="footer">Footer</el-footer>
+  </el-container>
 </template>
 
-
 <script lang="ts" setup>
-import { Menu as IconMenu, Message, Setting } from "@element-plus/icons-vue";
-import HelloWorld from "./components/HelloWorld.vue";
-import { ref } from "vue";
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import "swiper/css";
-
-
+// 引入图片
+// import img1 from '../assets/image/home_header_bgd.jpg';
+const localImage = new URL('../assets/image/home_header_bgd.jpg', import.meta.url).href;
+console.log(localImage);
+const urls = [
+  'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+  'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+  'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+];
 </script>
 
 <style>
-.app{
+.app {
   width: 100% !important;
 }
 </style>
 
 <style scoped>
 
+.navigator {
+ width: 100%;
+  height: 84px;
+  background-color: #ffffff;
+}
 
+.img {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+}
 
-.main{
+.el-image {
+  width: 100%;
+  /* height: 640px; */
+  object-fit: cover; 
+}
+.demo-image__lazy {
+  /* height: 400px; */
+  width: 100%;
+  overflow-y: auto;
+}
+.demo-image__lazy .el-image {
+  display: block;
+  min-height: 200px;
+  margin-bottom: 10px;
+}
+.demo-image__lazy .el-image:last-child {
+  margin-bottom: 0;
+}
+
+.main {
   width: 100%;
   display: block;
   height: 1000px;
   background-color: red;
 }
 
-.footer{
+.footer {
   display: block;
   width: 100%;
   height: 100px;
-  
   background-color: #3269b1;
 }
 
@@ -50,7 +89,7 @@ import "swiper/css";
   width: 100%;
 }
 
-.header{
+.header {
   height: 200px;
   background-color: #3269b1;
 }
