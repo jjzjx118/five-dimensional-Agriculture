5912abae43f1805dcc2cc73a9c163cdbbbce5ad0
jjzjx118 <jjzjx118@qq.com>
2025-03-03 04:56:13 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import path from 'path-browserify'; 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   base: "/five-dimensional-Agriculture/", // 你的仓库名
@@ -10,7 +14,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src') // 正确解析路径
     }
   }
 });
