e0fd30d7e2f400ae6b894a0b1b3609b2cdc530f7
Jinxin <jjzjx118@qq.com>
2025-03-03 18:11:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-  base: "/five-dimensional-Agriculture/", // 你的仓库名
+  base: "/", // 你的仓库名
   build: {
     outDir: "dist",
   },
